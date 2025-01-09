8ace58546692625d09db5525043c088583c1bb2d
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-09 19:44:11 +0530

    D6.java

--- Core java/D6.java
+++ Core java/D6.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
+
 public class D6 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter username");
-        String username = sc.next();
-        System.out.println("Enter password: ");
-        int Pass=sc.nextInt();
-        sc.close();
-        while (username == "srisai25" && Pass == 12345 ) {
-            System.out.println("HEllO"+username);
-            break;
-            
+        Scanner sc = new Scanner(System.in);
+        String username;
+        int pass;
+
+        while (true) {
+            System.out.println("Enter username:");
+            username = sc.next();
+            System.out.println("Enter password:");
+            pass = sc.nextInt();
+
+            if (username.equals("srisai25") && pass == 12345) {
+                System.out.println("Hello " + username);
+                break;
+            } else {
+                System.out.println("Invalid username or password. Please try again.");
+            }
         }
-       
-    }   
-    
+        sc.close();
+    }
 }
