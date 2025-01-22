22477e51eeba7ca387830e560eab72fd74ca1b94
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-22 12:32:52 +0530

    User Credentials
    User Credentials

--- Core java/D5.java
+++ Core java/D5.java
@@ -2,9 +2,7 @@ public class D5 {
     public static void main(String[] args) {
         String username ="srisai25";
         String pass ="12345";
-        String username ="achhu07";
-        String pass ="12345";
-        while (username == "achhu07" && pass == "12345" ) {
+        while (username == "srisai25" && pass == "12345" ) {
             System.err.println("HEllO");
             break;
             
