fb5af43bd13edb40da4998516e48681524bf6aac
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-22 12:28:55 +0530

    print 1-100 numbers using for loop
    print 1-100 numbers using for loop

--- Core java/D4.java
+++ Core java/D4.java
@@ -2,6 +2,6 @@ public class D4 {
     public static void main(String[] args) {
         int n;
         for(n=1;n<=100;n++)
-           System.out.println(n);
+           System.out.println("number: "+n);
     }
 }
