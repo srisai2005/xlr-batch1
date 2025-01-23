ee9bd297325dc6a9a517c88439e911d1196d63e5
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-23 23:40:17 +0530

    User details using while loop
    user details using while loop

---
+++ Core java/Demo007.java
@@ -0,0 +1,11 @@
+public class Demo007 {
+    public static void main (String[] args){
+        String username= "Srisai";
+        String pwd="12345";
+        while((username == "Srisai") && (pwd == "12345")) {
+            System.out.println("Hello Shiva you are welcome");
+            break;
+        }
+    }
+    
+}
