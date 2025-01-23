45ac98f90a8010e07876357bdecd2356ba0c55f8
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-23 23:47:27 +0530

    Area of Triangle
    Area of Triangle

---
+++ Core java/Demo013.java
@@ -0,0 +1,12 @@
+public class Demo013 {
+    public static double getTriangleArea(int s1, int s2, int s3) {
+        double s = (s1 + s2 + s3) / 2.0;
+        double ta = Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
+        return ta;
+    }
+    public static void main(String[] args) {
+        int s1 = 5, s2 = 6, s3 = 7;
+        double ta = getTriangleArea(s1, s2, s3);
+        System.out.println("The triangle area is: " + ta);
+    }
+}
