704d5807aca3427d20ba3fa0765153f3afee80a7
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-24 00:11:36 +0530

    Heterogeneous HashSet
    Heterogeneous HashSet

---
+++ Core java/Demo031.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+class HashSetDemo1 {
+	void HSetMethod() {
+		HashSet hashSet = new HashSet();
+		hashSet.add(100);
+		hashSet.add("Srisai");
+		hashSet.add(9999.99);
+		System.out.println(hashSet);		
+
+		for(Object x: hashSet) {
+			System.out.println(x);
+		}
+	}
+}
+public class Demo031 {
+	public static void main(String[] args) {
+		HashSetDemo1 obj = new HashSetDemo1();
+		obj.HSetMethod();
+		}
+	}
