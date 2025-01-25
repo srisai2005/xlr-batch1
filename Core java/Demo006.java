f0620e8cd976cabd0cc3870672ad3bdaa62ba989
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-25 11:27:03 +0530

    Diff Username and password

---
+++ Core java/Demo006.java
@@ -0,0 +1,13 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String username = "Srisai";
+   String pwd = "Im@Srisai";
+   while ((username=="Srisai") && ( pwd=="Im@Srisai")){
+    System.out.println("Hello "+username+",welcome to MRU ");
+    break;
+    
+   } 
+        
+    }
+    
+}
