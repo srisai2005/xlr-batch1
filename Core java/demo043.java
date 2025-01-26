59fbf850878d2d5e0bc1a33683f490e913d78a75
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-26 13:28:53 +0530

    Add files via upload

---
+++ Core java/demo043.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class demo043 {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        System.out.println(Character.toUpperCase(ch));
+        sc.close();
+        
+      
+
+
+    
+}
+}
+    
+
