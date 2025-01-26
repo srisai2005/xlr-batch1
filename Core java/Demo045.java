65bb477f0a0c9acc838dccd53deacd04ff2c88ce
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-26 13:34:03 +0530

    swap 2 numbers without 3rd variable

--- Core java/Demo045.java
+++ Core java/Demo045.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo045 {
+public class Demo045 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter value [a]:");
