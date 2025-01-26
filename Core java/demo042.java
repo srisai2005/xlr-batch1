a59f5d4ad5e338cb2c81b6a84f60734e940030d6
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-26 13:27:35 +0530

    Lowercase character into uppercase

---
+++ Core java/demo042.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class demo042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        System.out.println(Character.toLowerCase(ch));
+        sc.close();
+        
+      
+
+
+    
+}
+}
