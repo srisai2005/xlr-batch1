0f77b8272b2ab525d7f0045224a7a289a0a506d8
srisai <143951410+srisai2005@users.noreply.github.com>
2025-01-26 13:31:50 +0530

    Swap of 2 numbers with 3rd variable

--- Core java/Demo044.java
+++ Core java/Demo044.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class demo044 {
+public class Demo044 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter value of [a]:");
