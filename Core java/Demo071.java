b4f6abc036f44cac9500a9082caf3fccca49b9fb
srisai <143951410+srisai2005@users.noreply.github.com>
2025-02-16 18:22:39 +0530

    Reverse Array Elements

---
+++ Core java/Demo071.java
@@ -0,0 +1,31 @@
+public class Demo071 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        System.out.println("Original array:");
+        printArray(numbers);
+        
+        reverseArray(numbers);
+        
+        System.out.println("Reversed array:");
+        printArray(numbers);
+    }
+
+    public static void reverseArray(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
