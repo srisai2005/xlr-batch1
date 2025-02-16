3ea0ab96cdceb0d32990c63ba0532511dc769772
srisai <143951410+srisai2005@users.noreply.github.com>
2025-02-16 18:19:13 +0530

    Print Index Values of an Array.

---
+++ Core java/Demo066.java
@@ -0,0 +1,8 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index: " + i + ", Value: " + arr[i]);
+        }
+    }
+}
