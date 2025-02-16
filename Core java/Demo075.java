950a87b6730e706619d81204e8122de2190658a2
srisai <143951410+srisai2005@users.noreply.github.com>
2025-02-16 18:26:08 +0530

    To Find Maximum Number of an Array.

---
+++ Core java/Demo075.java
@@ -0,0 +1,40 @@
+class MaximumNO{
+    int[] array = {10, 20, 30, 40, 50};
+
+    void display(){
+        System.out.println("Original array: ");
+        printArray(array);
+
+        int max = findMax(array);
+
+        System.out.println("Maximum number: " + max);
+    }
+
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+public class Demo075 {
+    public static void main(String[] args) {
+        MaximumNO obj = new MaximumNO();
+        obj.display();
+    }
+
+    
+}
+
